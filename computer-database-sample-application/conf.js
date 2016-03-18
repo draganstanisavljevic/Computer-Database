cfbd45476e18079377b75fded7db00d456c12ee3
Dragan Stanisavljevic <Dragan.Stanisavljevic@tomtom.com>
2016-03-18 11:38:06 +0200

    Added package.json file

--- computer-database-sample-application/conf.js
+++ computer-database-sample-application/conf.js
@@ -1,6 +1,5 @@
-// conf.js
 exports.config = {
   framework: 'jasmine2',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['specs/table_ordering_spec.js', ]
+  specs: ['specs/main_page_spec.js', 'specs/add_new_computer_view_spec.js', 'specs/add_new_computer_validation_spec.js', 'specs/add_new_computer_submit_valid_data_spec.js', 'specs/table_ordering_spec.js']
 }
